32a9df6261ebd4760d3862f0f1e046f0c04a9096
WiiruxCruz <hialf.92@gmail.com>
2021-11-23 17:29:36 -0600

    Creacion de services historial y listado de items

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
 
-  constructor() { }
+  constructor(private gifsService: GifsService) { }
 
-  ngOnInit(): void {
+  get historial() {
+    //regresar el historial que está en el servicio
+    return this.gifsService.historial;
   }
 
 }
